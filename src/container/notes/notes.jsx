d1b8ae0d66cc380ef82e9f1051b0bffa38b2a6a8
Wildan Asyrof Dewayana <asyrof.wildan@gmail.com>
2022-06-10 00:19:37 +0700

    notes delete

--- src/container/notes/notes.jsx
+++ src/container/notes/notes.jsx
@@ -5,7 +5,8 @@ import Topbar from "../../components/menubar/topbar"
 import './notes.css';
 import Note from "../../components/note"
 import { auth, db, dbf } from '../../firebase';
-import { addDoc, collection, doc, getDocs, setDoc } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, getDocs, setDoc } from 'firebase/firestore';
+import { async } from '@firebase/util';
 
 export class Notes extends Component {
 
@@ -31,8 +32,8 @@ export class Notes extends Component {
         try {
             const querySnapshot = await getDocs(collection(db, "notes", auth.currentUser.uid, "items"));
             querySnapshot.forEach((doc) => {
-                list.push({...doc.data(), id: doc.id});
-            });            
+                list.push({ ...doc.data(), id: doc.id });
+            });
             console.log(list);
             this.setState({
                 allData: list
@@ -44,8 +45,8 @@ export class Notes extends Component {
         }
     }
 
-    componentDidMount(){
-        
+    componentDidMount() {
+
         this.fetchData();
         console.log(this.data);
     }
@@ -59,6 +60,13 @@ export class Notes extends Component {
         this.setState(state);
     }
 
+    handleDelete = async (id) => {
+        deleteDoc(doc(db, "notes", auth.currentUser.uid, "items", id))
+            .then(
+                this.fetchData()
+            )
+    }
+
     onSubmit = async (e) => {
         e.preventDefault();
 
@@ -68,13 +76,16 @@ export class Notes extends Component {
             "body": body,
             "date": date
         })
-        .then((docRef) => {
-            this.setState({
-                title: "",
-                body: "",
-                date: ""
+            .then(
+                this.fetchData()
+            )
+            .then((docRef) => {
+                this.setState({
+                    title: "",
+                    body: "",
+                    date: ""
+                })
             })
-        })
 
         console.log(res);
     }
@@ -150,21 +161,26 @@ export class Notes extends Component {
             var title = val.title
             var body = val.body
             var date = val.date
+            var id = val.id
             return (
-                <div key={{i}} className="list-notes card-notes mb-3">
-            <div className="card-body my-4 mx-3">
-                <button className="btn-delete float-end"
-                    >
-                    <ion-icon name="close-outline"></ion-icon>
-                </button>
-                <h5 className="card-title float-none">{title}</h5>
-                <h6>{date}</h6>
-                <p>{body}</p>
-            </div>
-        </div>
-            )            
+                <div key={{ i }} className="list-notes card-notes mb-3">
+                    <div className="card-body my-4 mx-3">
+                        <button className="btn-delete float-end"
+                            onClick={
+                                () => {
+                                    this.handleDelete(id)
+                                }
+                            }>
+                            <ion-icon name="close-outline"></ion-icon>
+                        </button>
+                        <h5 className="card-title float-none">{title}</h5>
+                        <h6>{date}</h6>
+                        <p>{body}</p>
+                    </div>
+                </div>
+            )
         })
-        const { title, body, date } = this.state;
+
         return (
             <div>
                 <Sidebar />
@@ -195,7 +211,7 @@ export class Notes extends Component {
 
                         <div className="row mt-3">
                             <div className="col-md-7 col-sm">
-                                <div className="row">                                    
+                                <div className="row">
                                     {listofData}
                                 </div>
                             </div>
