7e1b817d6ca53e9f92c52d01ddbe2e78ebaf2d69
Khoirunnisa <k.nisa11602@gmail.com>
2022-06-10 00:40:35 +0700

    add todolist crud

--- src/container/todolist/todolist.jsx
+++ src/container/todolist/todolist.jsx
@@ -3,8 +3,81 @@ import Sidebar from "../../components/menubar/sidebar";
 import Topbar from "../../components/menubar/topbar";
 import Todo from "../../components/todo";
 import './todolist.css';
+import { auth, db, dbf } from '../../firebase';
+import { addDoc, collection, doc, getDocs, deleteDoc } from 'firebase/firestore';
 
 class Todolist extends Component {
+
+    constructor() {
+        super();
+        // this.userUid = auth.currentUser.uid;
+        // this.ref = db.firestore().collection('notes').doc(this.userUid).collection('items');
+        // this.ref = db.collection('notes').doc(auth.currentUser.uid).collection('items');
+        this.user = auth.currentUser.uid;
+        this.state = {
+            allData: [],
+            "todos": '',
+            "status": '',
+        };
+    }
+
+    fetchData = async () => {
+        var list = [];
+        try {
+            const querySnapshot = await getDocs(collection(db, "todolist", auth.currentUser.uid, "items"));
+            querySnapshot.forEach((doc) => {
+                list.push({...doc.data(), id: doc.id});
+            });            
+            console.log(list);
+            this.setState({
+                allData: list
+            })
+            this.state.allData = list;
+            console.log(this.state.allData)
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    componentDidMount(){
+        
+        this.fetchData();
+        console.log(this.data);
+    }
+
+    onChange = (e) => {
+        const state = this.state;
+        state[e.target.name] = e.target.value;
+        state['status'] = 'false';
+        this.setState(state);
+    }
+
+    handleDelete = async (id) => {
+        deleteDoc(doc(db, "todolist", auth.currentUser.uid, "items", id))
+            .then(
+                this.fetchData()
+            )
+    }
+
+    onSubmit = async (event) => {
+        event.preventDefault();
+
+        const { todos, status } = this.state;
+        const res = await addDoc(collection(db, "todolist", auth.currentUser.uid, "items"), {
+            "todos": todos,
+            "status": status
+        })
+        .then(this.fetchData)
+        .then((docRef) => {
+            this.setState({
+                todos: "",
+                status: "",
+            })
+        })
+
+        console.log(res);
+    }
+
     // state = {
     //     todolist: [],
     //     insertTodo: {
@@ -77,6 +150,27 @@ class Todolist extends Component {
     //         .then(json => this.fetchTodolist())
     // }
     render() {
+        var listofData = this.state.allData.map((val, i) => {
+            var todos = val.todos
+            var status = val.status
+            var id = val.id
+            return(
+                <div key={{ i }} className="form-check mx-3 mb-4">
+            <input className="form-check-input" type="checkbox" value="status" id="status" />
+            <label className="form-check-label ms-3" for="todolist1">
+                {todos}
+            </label>
+            <button className="btn-delete float-end" 
+            onClick={
+                () => {
+                    this.handleDelete(id)
+                }
+            }>
+                <ion-icon name="trash-outline"></ion-icon>
+            </button>
+        </div>
+            )
+        })
         return (
             <div>
                 <Sidebar />
@@ -88,12 +182,12 @@ class Todolist extends Component {
                         <div className="add-todolist">
                             <div className="card-todolist">
                                 <div className="card-body">
-                                    <form action="submit" className="m-2 row">
+                                    <form className="m-2 row">
                                         <div className="col">
-                                            <input type="text" className="form-control px-4" name="nama_todo" id="nama_todo" placeholder="Tambahkan to do list baru di sini ..." onChange={this.handleChangeInsert} />
+                                            <input type="text" className="form-control px-4" name="todos" id="todos" placeholder="Tambahkan to do list baru di sini ..." onChange={this.onChange} />
                                         </div>
                                         <div className="col-md-auto col-sm align-self-center">
-                                            <button className="btn btn-danger d-inline-block" onClick={this.insertTodolist}> + Tambah</button>
+                                            <button className="btn btn-danger d-inline-block" onClick={this.onSubmit}> + Tambah</button>
                                         </div>
                                     </form>
                                 </div>
@@ -107,10 +201,11 @@ class Todolist extends Component {
                                         <h2 className="mb-4">Today's To-dos</h2>
                                         </div>
                                         <div className="col text-end">
-                                            <button className="btn btn-light" onClick={this.deleteAllTodolist}>Hapus semua</button>
+                                            <button className="btn btn-light" onClick={this.handleDelete}>Hapus semua</button>
                                         </div>
                                     </div>
                                     <div className="row">
+                                        {listofData}
                                     </div>
                                 </div>
                             </div>
