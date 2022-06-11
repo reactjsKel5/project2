b09f0f87941bcae9bd54ee92a89624a26036c13d
pujirahayu01 <puji89503@gmail.com>
2022-06-12 06:23:14 +0700

    melakukan update income firebase dan FE

--- src/container/income/income.jsx
+++ src/container/income/income.jsx
@@ -2,12 +2,92 @@ import React, { Component } from "react";
 import IncomeList from "../../components/incomeList";
 import Sidebar from "../../components/menubar/sidebar";
 import Topbar from "../../components/menubar/topbar";
+import { auth, db } from '../../firebase';
+import { addDoc, collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
 import './income.css';
 import {
     Link
 } from "react-router-dom";
+import { async } from "@firebase/util";
 
 class Income extends Component {
+    constructor() {
+        super();
+
+        this.user = auth.currentUser.uid;
+        this.data = [];
+        this.state = {
+            allData: [],
+            'category': '',
+            'date': '',
+            'income': '',
+            'title': '',
+        };
+    }
+
+    fetchData = async () => {
+        var list = [];
+        try {
+            const querySnapshot = await getDocs(collection(db, "income", auth.currentUser.uid, "items"));
+            querySnapshot.forEach((doc) => {
+                list.push({ ...doc.data(), id: doc.id });
+            });
+            console.log(list);
+            this.setState({
+                allData: list
+            })
+            this.state.allData = list;
+            console.log(this.state.allData)
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    handleDelete = async (id) => {
+        deleteDoc(doc(db, "income", auth.currentUser.uid, "items", id))
+            .then(
+                this.fetchData()
+            )
+    }
+
+    componentDidMount() {
+
+        this.fetchData();
+        console.log(this.data);
+    }
+
+    onChange = (e) => {
+        const state = this.state
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    }
+
+
+    onSubmit = async (e) => {
+        e.preventDefault();
+
+        const { category, date, income, title } = this.state;
+        const res = await addDoc(collection(db, "income", auth.currentUser.uid, "items"), {
+            "category": category,
+            "date": date,
+            "income": income,
+            "title": title
+        })
+            .then(
+                this.fetchData()
+            )
+            .then((docRef) => {
+                this.setState({
+                    category: "",
+                    date: "",
+                    income: "",
+                    title: ""
+                })
+            })
+        console.log(res);
+    }
+
+
+
     // state = {
     //     listIncome: [],
     //     addIncome: {
@@ -81,6 +161,38 @@ class Income extends Component {
     // }
 
     render() {
+        var listofData = this.state.allData.map((val, i) => {
+            var category = val.category
+            var date = val.date
+            var income = val.income
+            var title = val.title
+            var id = val.id
+
+            return (
+                <div className="income-item row mt-4">
+                    <div className="col-auto">
+
+                    </div>
+                    <div className="col nama-pemasukan align-self-center">
+                        <p className="m-0">{title}</p>
+                    </div>
+                    <div className="col jumlah align-self-center">
+                        <p className="m-0">{income}</p>
+
+                    </div>
+                    <div className="col-auto delete align-self-center">
+                        <button onClick={
+                            () => {
+                                this.handleDelete(id)
+                            }
+                        }>
+                            <ion-icon name="trash-outline"></ion-icon>
+                        </button>
+                    </div>
+                </div>
+
+            )
+        })
         return (
             <div>
                 <Sidebar />
@@ -102,23 +214,80 @@ class Income extends Component {
                                 </div>
                             </div>
                         </div>
+
+                        {/* untuk saldo balance */}
+                        <div className="card-saldo-balance mb-5">
+                            <div className="card-body mx-4 my-3">
+                                <div className="row">
+                                    <div className="col title-balance-right text-center d-flex justify-content-center">
+                                        <button href="#" className="arrow float-end"><ion-icon name="chevron-back-outline"></ion-icon></button>
+
+                                    </div>
+                                    <div className="col date-balance text-center d-flex justify-content-center">
+                                        <h5> June 2022</h5>
+                                    </div>
+
+                                    <div className="col title-balance-left text-center d-flex justify-content-center">
+                                        <button href="#" className="arrow float-end"><ion-icon name="chevron-forward-outline"></ion-icon></button>
+
+                                    </div>
+                                    <div>
+                                        <hr className="mt-2 text-center d-flex justify-content-center text-center" />
+                                    </div>
+                                </div>
+                                <h1 className="text-center mt-1 mb-2">Rp 3.245.500</h1>
+                                <div className="row  ">
+                                    <div className="col pemasukan align-self-center justify-content-center text-center mt-2">
+                                        <h5>Income</h5>
+                                        <h3>Rp 5.000.000</h3>
+                                    </div>
+                                    <div className="col pengeluaran align-self-center justify-content-center text-center mt-3">
+                                        <h5>Outcome</h5>
+                                        <h3>Rp 1.754.500</h3>
+
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
                         <div className="card-information-insert mb-5">
                             <div className="card-body mx-4 my-3">
                                 <div className="row">
                                     <div className="col-md-auto col-sm">
-                                        <h3>Chart</h3>
-                                        <h5>Presentase pemasukan</h5>
-                                        <div className="row d-flex mt-4">
-                                            <div className="col-md-auto col-sm">
-                                                <img src={require('./advanced.png')} alt="chart" />
+                                        <div className="card-total-pemasukan mb-3">
+                                            <div className="card-body mx-2 my-1">
+                                                <h5>Total pemasukan</h5>
+                                                <div className="row  ">
+                                                    <div className="col nominal mt-2">
+                                                        <h3>135.000</h3>
+                                                    </div>
+                                                    <div className="col rupiah mt-3">
+                                                        <h5>Rupiah</h5>
+                                                    </div>
+
+                                                </div>
                                             </div>
-                                            <div className="col align-self-center">
-                                                <ul>
-                                                    <li>Gaji</li>
-                                                    <li>Orang Tua</li>
-                                                    <li>Hadiah</li>
-                                                    <li>Investasi</li>
-                                                </ul>
+                                        </div>
+
+
+
+                                        <div className="card-presentase mb-2">
+                                            <div className="card-body mx-2 my-1">
+                                                <h3>Chart</h3>
+                                                <h5>Presentase pemasukan</h5>
+                                                <div className="row d-flex mt-4">
+                                                    <div className="col-md-auto col-sm">
+                                                        <img src={require('./advanced.png')} alt="chart" />
+                                                    </div>
+                                                    <div className="col align-self-center">
+                                                        <ul>
+                                                            <li>Gaji</li>
+                                                            <li>Orang Tua</li>
+                                                            <li>Hadiah</li>
+                                                            <li>Investasi</li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -132,16 +301,17 @@ class Income extends Component {
                                             </div>
                                         </div>
                                         <form action="submit">
-                                            <select className="form-control category-select mb-3" name="nama_kategori_pemasukan" id="nama_kategori_pemasukan" onChange={this.handleAdd} >
-                                                {/* {
-                                                    this.state.kategori.map(cat => {
-                                                        return <option value={cat.nama_kategori_pemasukan}>{cat.nama_kategori_pemasukan}</option>
-                                                    })
-                                                } */}
+                                            <select className="form-control category-select mb-3" name="category" id="category" onChange={this.onChange} >
+                                                <option value="0">--</option>
+                                                <option value="gaji">Gaji</option>
+                                                <option value="orangtua">Orang Tua</option>
+                                                <option value="hadiah">Hadiah</option>
+                                                <option value="investasi">Investasi</option>
+                                                <option value="lain">Lain-Lain</option>
                                             </select>
-                                            <input type="number" className="form-control px-4 mb-3" name="pemasukan" id="pemasukan" placeholder="Jumlah (Rp.)" onChange={this.handleAdd} />
-                                            <input type="text" className="form-control px-4 mb-5" name="catatan" id="catatan" placeholder="Catatan" onChange={this.handleAdd} />
-                                            <button className="btn btn-danger d-inline-block" onClick={this.insertIncome}>Tambah</button>
+                                            <input type="number" className="form-control px-4 mb-3" name="income" id="income" placeholder="Jumlah (Rp.)" onChange={this.onChange} />
+                                            <input type="text" className="form-control px-4 mb-5" name="title" id="title" placeholder="Catatan" onChange={this.onChange} />
+                                            <button className="btn btn-danger d-inline-block" onClick={this.onSubmit}>Tambah</button>
                                         </form>
                                     </div>
                                 </div>
@@ -152,6 +322,7 @@ class Income extends Component {
                             <div className="card-income-list mb-3">
                                 <div className="card-body m-3">
                                     <small>28/02/2022</small>
+                                    {listofData}
                                     {/* {
                                         this.state.listIncome.map(data => {
                                             return <IncomeList key={data.id} uid={data.uid} pemasukan={data.pemasukan} catatan={data.catatan} id={data.id} delete={this.handleDelete} />
