b8e4e374be4de318412baf96300823a9c556b68d
rfkrta <rafikartaw2@gmail.com>
2022-06-15 08:08:53 +0700

    Insert Schedule

--- src/container/schedule/schedule.jsx
+++ src/container/schedule/schedule.jsx
@@ -3,9 +3,89 @@ import Sidebar from "../../components/menubar/sidebar";
 import Topbar from "../../components/menubar/topbar";
 import ScheduleProps from "../../components/scheduleprops";
 import './schedule.css';
-import { addDoc, collection } from "firebase/firestore"; 
+// import { addDoc, collection } from "firebase/firestore";
+import { auth, db, dbf } from '../../firebase';
+import { addDoc, collection, deleteDoc, doc, getDocs, setDoc } from 'firebase/firestore';
+import { async } from '@firebase/util';
 
 class Schedule extends Component {
+
+
+    constructor() {
+        super();
+        // this.userUid = auth.currentUser.uid;
+        // this.ref = db.firestore().collection('notes').doc(this.userUid).collection('items');
+        // this.ref = db.collection('notes').doc(auth.currentUser.uid).collection('items');
+        this.user = auth.currentUser.uid;
+        this.day = "Selasa"
+        this.state = {
+            allData: [],
+            'day': '',
+            "timeend": '',
+            "timestart": '',
+            "topic": '',
+        };
+    }
+    
+    onChange = (e) => {
+        const state = this.state
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    }
+
+    fetchData = async () => {
+        var list = [];
+        try {
+            const querySnapshot = await getDocs(collection(db, "schedule", auth.currentUser.uid, this.day));
+            querySnapshot.forEach((doc) => {
+                list.push({ ...doc.data(), id: doc.id });
+            });
+            console.log(list);
+            this.setState({
+                allData: list
+            })
+            this.state.allData = list;
+            console.log(this.state.allData)
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+
+
+    componentDidMount() {
+
+        this.fetchData();
+        console.log(this.data);
+    }
+
+    onSubmit = async (e) => {
+        e.preventDefault();
+        const { day, timeend, timestart, topic } = this.state;
+        const res = await addDoc(collection(db, "schedule", auth.currentUser.uid, day), {
+            "day": day,
+            "timeend": timeend,
+            "timestart": timestart,
+            "topic": topic
+        })
+        .then(
+            this.fetchData()
+        )
+            .then((docRef) => {
+                this.setState({
+                    day: "",
+                    timeend: "",
+                    timestart: "",
+                    topic: ""
+                })
+            })
+        console.log(res);
+    }
+    
+    onChangeSelect = (day) => {
+        this.day = day;
+        console.log(this.day);
+    }
 
     // state = {
     //     schedule: [],
@@ -76,9 +156,30 @@ class Schedule extends Component {
     // }
 
     render() {
+
+        var listofData = this.state.allData.map((val, i) => {
+            var day = val.day
+            var timeend = val.timeend
+            var timestart = val.timestart
+            var topic = val.topic
+            return (
+                <div class="row text-secondary mb-1">
+                    <div class="col-4">
+                        <p>{timestart} - {timeend}</p>
+                    </div>
+                    <div class="col-6">
+                        <p>{topic}</p>
+                    </div>
+                    <div class="col-2">
+                        <button className="btn-delete float-end">
+                            <ion-icon name="close-outline"></ion-icon>
+                        </button>
+                    </div>
+                </div>
+            )
+        })
+
         return (
-
-
             <div>
                 <Sidebar />
                 {/* Tulis content di bawah sini */}
@@ -94,34 +195,35 @@ class Schedule extends Component {
                                         <div className="row">
                                             <div class="col-md">
                                                 <div class="mb-4">
-                                                    <label for="day" class="text-secondary">Hari</label>
-                                                    <select id="inputHari" class="form-select">
-                                                        <option>Senin</option>
-                                                        <option>Selasa</option>
-                                                        <option>Rabu</option>
-                                                        <option>Kamis</option>
-                                                        <option>Jumat</option>
-                                                        <option>Sabtu</option>
-                                                        <option>Minggu</option>
+                                                    <label for="dey" class="text-secondary">Hari</label>
+                                                    <select id="day" name="day" class="form-select" onChange={this.onChange} value={this.state[this.day]}>
+                                                        <option>...</option>
+                                                        <option value="Senin">Senin</option>
+                                                        <option value="Selasa">Selasa</option>
+                                                        <option value="Rabu">Rabu</option>
+                                                        <option value="Kamis">Kamis</option>
+                                                        <option value="Jumat">Jumat</option>
+                                                        <option value="Sabtu">Sabtu</option>
+                                                        <option value="Minggu">Minggu</option>
                                                     </select>
                                                 </div>
                                                 <div class="mb-4">
                                                     <label for="namaJadwal" class="text-secondary">Nama Jadwal</label>
-                                                    <input type="text" class="form-control" name="nama_schedule" id="nama_schedule" placeholder="..."  />
+                                                    <input type="text" class="form-control" name="topic" id="topic" placeholder="..." onChange={this.onChange} />
                                                 </div>
                                             </div>
                                             <div class="col-md">
                                                 <div class="mb-4">
                                                     <label for="waktuMulai" class="text-secondary">Waktu Mulai</label>
-                                                    <input type="time" class="form-control" name="waktu_mulai" id="waktu_mulai"  />
+                                                    <input type="time" class="form-control" name="timestart" id="timeend" onChange={this.onChange} />
                                                 </div>
                                                 <div class="mb-4">
                                                     <label for="waktuBerakhir" class="text-secondary">Waktu Berakhir</label>
-                                                    <input type="time" class="form-control" name="waktu_berakhir" id="waktu_berakhir"/>
+                                                    <input type="time" class="form-control" name="timeend" id="timeend" onChange={this.onChange} />
                                                 </div>
                                                 <div className="row">
                                                     <div className="col-md-12 text-end" >
-                                                        <button className="btn-tambah">Tambah</button>
+                                                        <button className="btn-tambah" onClick={this.onSubmit}>Tambah</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -131,26 +233,32 @@ class Schedule extends Component {
                             </div>
                         </div>
                         <div class="section-nav-hari">
-                            <nav class="nav nav-day my-5">
-                                <a class="nav-link active" aria-current="page" href="#">Senin</a>
-                                <a class="nav-link" href="#">Selasa</a>
-                                <a class="nav-link" href="#">Rabu</a>
-                                <a class="nav-link" href="#">Kamis</a>
-                                <a class="nav-link" href="#">Jumat</a>
-                                <a class="nav-link" href="#">Sabtu</a>
-                                <a class="nav-link" href="#">Minggu</a>
-                            </nav>
+                            {/* <nav class="nav nav-day my-5">
+                                <a class="nav-link active" aria-current="page"  onClick={this.onChangeSelect('Senin')}>Senin</a>
+                                <a class="nav-link" onClick={this.onChangeSelect('Selasa')}>Selasa</a>
+                                <a class="nav-link" href="#" onClick={this.onChangeSelect('Rabu')}>Rabu</a>
+                                <a class="nav-link" href="#" onClick={this.onChangeSelect('Kamis')}>Kamis</a>
+                                <a class="nav-link" href="#"onClick={this.onChangeSelect('Jumat')}>Jumat</a>
+                                <a class="nav-link" href="#" onClick={this.onChangeSelect('Sabtu')}>Sabtu</a>
+                                <a class="nav-link" href="#" onClick={this.onChangeSelect('Minggu')}>Minggu</a>
+                            </nav> */}
                         </div>
                         <div className="col-sm">
                             <div className="card-schedule">
                                 <div className="card-body my-4 mx-3">
-                                        <div class="card-body text-light">
-                                            <div class="judul row title-schedule mb-5">
-                                                <div class="col-4">Waktu</div>
-                                                <div class="col-6">Jadwal</div>
-                                                <div class="col-2"></div>
+                                    <div class="card-body text-light">
+                                        <div class="judul row title-schedule mb-5">
+                                            <div class="col-4">Waktu</div>
+                                            <div class="col-6">Jadwal</div>
+                                            <div className="row">
+                                            <div class="col">
+                                               <p>
+                                               {listofData}
+                                               </p>
+                                            </div>
                                             </div>
                                         </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
