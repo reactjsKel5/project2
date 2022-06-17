6fec792d538861647ce3595db381c6a442f0b547
NabiylaHamestuti14 <nbylhmstt@gmail.com>
2022-06-17 10:54:16 +0700

    new update

--- src/container/profil/profil.jsx
+++ src/container/profil/profil.jsx
@@ -28,7 +28,7 @@ class Profil extends Component {
     fetchData = async () => {
         var list = [];
         try {
-            const querySnapshot = await getDocs(collection(db, "profile", auth.currentUser.uid, "items"));
+            const querySnapshot = await getDocs(collection(db, "users", auth.currentUser.uid, "items"));
             querySnapshot.forEach((doc) => {
                 list.push({...doc.data(), id: doc.id});
             });            
@@ -105,7 +105,7 @@ class Profil extends Component {
         })
 
         var listofDataProfile = this.state.allData.map((val, i) => {
-            var nama = val.nama
+            var nama_lengkap = val.nama_lengkap
         return (
             <div className="topbar">
             <div className="toggle">
@@ -113,7 +113,7 @@ class Profil extends Component {
             </div>
             <div className="user-information row">
                 <div className="col name align-self-center">
-                    <h6>{nama}</h6>
+                    <h6>{nama_lengkap}</h6>
                 </div>
                 <div className="col user">
                 <img src="https://images.unsplash.com/photo-1638204957796-4ad60705aa17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHBvcnRyYWl0JTIwcGhvdG9ncmFwaHl8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" width="200" alt="user-photo" />
--- src/container/task/task.jsx
+++ src/container/task/task.jsx
@@ -47,7 +47,7 @@ class Task extends Component {
     fetchDataProfile = async () => {
         var list = [];
         try {
-            const querySnapshot = await getDocs(collection(db, "profile", auth.currentUser.uid, "items"));
+            const querySnapshot = await getDocs(collection(db, "users", auth.currentUser.uid, "items"));
             querySnapshot.forEach((doc) => {
                 list.push({ ...doc.data(), id: doc.id });
             });
@@ -214,7 +214,7 @@ class Task extends Component {
         })
 
         var listofDataProfile = this.state.allDataProfile.map((val, i) => {
-            var nama = val.nama
+            var nama_lengkap = val.nama_lengkap
         return (
             <div className="topbar">
             <div className="toggle">
@@ -222,7 +222,7 @@ class Task extends Component {
             </div>
             <div className="user-information row">
                 <div className="col name align-self-center">
-                    <h6>{nama}</h6>
+                    <h6>{nama_lengkap}</h6>
                 </div>
                 <div className="col user">
                     <img src="https://images.unsplash.com/photo-1638204957796-4ad60705aa17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHBvcnRyYWl0JTIwcGhvdG9ncmFwaHl8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" width="200" alt="user-photo" />
